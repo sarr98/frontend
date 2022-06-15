00df27e2e5ad7e22fa77d08365d03cb2e9f09c5e
abdoulaye <abdoulaye.sarr3@ism.edu.sn>
2022-06-15 16:21:13 +0000

    coomit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Router} from "@angular/router";
+import {ProductModelServer, ServerResponse} from "../../models/product.model";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  products: ProductModelServer[] = [];
+
+  constructor(private productService: ProductService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((prods:ServerResponse)=>{
+      this.products=prods.products;
+      console.log(prods);
+    });
+  }
+  selectProduct(id: Number){
+    this.router.navigate(['/product' , id]).then();
+
+  }
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {ProductModelServer, ServerResponse} from "../models/product.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private SERVER_URL= environment.SERVER_URL;
+
+  constructor(private http: HttpClient) { }
+  /*This is to fecth all products from the backend server*/
+  getAllProducts(numberOfResults=10):Observable<ServerResponse>{
+    console.log(this.SERVER_URL + '/products');
+    return this.http.get<ServerResponse>(this.SERVER_URL + '/products', {
+      params:{
+        limit: numberOfResults.toString()
+      }
+    });
+  }
+  /*GET SINGLE PRODUCT FROM SERVER */
+  getSingleProd(id:number):Observable<ProductModelServer>{
+
+    return this.http.get<ProductModelServer>(this.SERVER_URL + '/products' + id);
+  }
+}
